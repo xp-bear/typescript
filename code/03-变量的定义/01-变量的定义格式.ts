12705d923bcfc084008bcb93c6ae47d2e41b995e
想走过亚洲的熊 <1693889638@qq.com>
2022-08-07 21:24:01 +0800

    变量的定义

---
+++ code/03-变量的定义/01-变量的定义格式.ts
@@ -0,0 +1,16 @@
+var name: string = "why";
+let age: number = 18;
+const height: number = 1.88;
+
+//string:TypeScript中的字符串类型
+//String:JavaScript的字符串包装类的类型
+
+//默认情况下进行赋值时，会将赋值的值的类型，作为前面的标识符的类型
+//这个过程称之为类型推导/推断
+//foo没有添加类型注解
+
+/*
+let foo = "foo";
+foo = 123;
+*/
+export {};
